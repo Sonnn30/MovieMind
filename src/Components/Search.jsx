c67243e5ee2a24723d36b05000f43c18bae1b1b7
Wilson P <wilsonprajnawira345@gmail.com>
2025-06-08 21:22:40 +0700

    Update Backend

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -61,7 +61,7 @@ function Search() {
     };
     // fetch backend
     try {
-      const res = await fetch("http://127.0.0.1:5000/api/predict", {
+      const res = await fetch("https://sps8gxlt-5000.asse.devtunnels.ms/api/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(requestBody),
